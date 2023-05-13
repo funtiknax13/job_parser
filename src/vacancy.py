cbb5106c9db84351b6c021865539f259a2f68ac8
funtiknax13 <sergeyfedorov13@yandex.ru>
2023-05-13 18:18:40 +0300

    create class HH/SJAPI

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from abc import ABC, abstractmethod
+from keys import SuperJobAPIKey
 
 
 class JobFindAPI(ABC):
@@ -15,64 +16,95 @@ class HeadHunterAPI(JobFindAPI):
     def get_vacancies(self, name: str) -> list:
         params = {
             'text': name,  # Имя вакансии для поиска
-            # 'area': 1,  # Расположение вакансии (1 -  Москва)
             'page': 0,  # Индекс страницы поиска на HH
             'per_page': 100  # Кол-во вакансий на 1 странице
         }
-        req = requests.get("https://api.hh.ru/vacancies", params)
-        data = json.loads(req.content.decode())
+        response = requests.get("https://api.hh.ru/vacancies", params)
+        data = json.loads(response.content.decode())
         vacancies = []
-        for item in data["items"][0:7]:
+        for item in data["items"]:
             name = item["name"]
-            area = item["area"]["name"]
             experience = item["experience"]["name"]
             link = "https://hh.ru/vacancy/" + item["id"]
-            vacancy = Vacancy(name, area, link)
-            vacancies.append(vacancy)
+            salary = item["salary"]
+            if salary:
+                salary_from = item["salary"]["from"]
+                if not salary_from:
+                    salary_from = 0
+                salary_to = item["salary"]["to"]
+                if not salary_to:
+                    salary_to = 0
+                salary_cur = item["salary"]["currency"].lower()
+                if salary_cur == "rur":
+                    salary_cur = "rub"
+            else:
+                salary_from = 0
+                salary_to = 0
+                salary_cur = "rub"
+            salary = f"{salary_from}-{salary_to} {salary_cur}"
+            vacancy = {
+                "name": name,
+                "link": link,
+                "salary": salary,
+                "experience": experience
 
-        # test = data["items"][5]
-        # print(test)
-        # id = test["id"]
-        # name = test["name"]
-        # area = test["area"]["name"]
-        # experience = test["experience"]["name"]
-        # link = "https://hh.ru/vacancy/" + test["id"]
-        # salary = test["salary"]
-        # print(f"Вакансия: {name}\nРасположение: {area}\nТребуемый опыт: {experience}\nСсылка: {link}")
-        # if salary:
-        #     salary_from = test["salary"]["from"]
-        #     salary_to = test["salary"]["to"]
-        #     salary_cur = test["salary"]["currency"]
-        #     print(f"Зарплата: от {salary_from} до {salary_to} {salary_cur}")
-        # else:
-        #     print("Зарплата: не указана")
-        #     salary_from = 0
-        #     salary_from = 0
+            }
+            vacancies.append(vacancy)
         return vacancies
 
 
 class SuperJobAPI(JobFindAPI):
 
+    # SUPERJOB_API_KEY = os.environ.get('SUPERJOB_API_KEY')
+    SUPERJOB_API_KEY = SuperJobAPIKey
+
     def get_vacancies(self, name: str) -> list:
-        pass
+        headers = {'X-Api-App-Id': self.SUPERJOB_API_KEY}
+        vacancies_count = 100
+        params = {'count': vacancies_count,
+                  'keyword': name}
+        response = requests.get('https://api.superjob.ru/2.0/vacancies/',
+                                    params=params, headers=headers)
+        data = json.loads(response.content.decode())
+        vacancies = []
+        for item in data["objects"]:
+            name = item["profession"]
+            salary_from = item["payment_from"]
+            salary_to = item["payment_to"]
+            salary_cur = item["currency"].lower()
+            experience = item["experience"]["title"]
+            link = item["link"]
+            salary = f"{salary_from}-{salary_to} {salary_cur}"
+            vacancy = {
+                "name": name,
+                "link": link,
+                "salary": salary,
+                "experience": experience
+            }
+            vacancies.append(vacancy)
+
+        return vacancies
 
 
 class Vacancy:
 
-    def __init__(self, name: str, area: str, link: str) -> None:
+    def __init__(self, name: str, salary: str, experience: str, link: str) -> None:
         self.name = name
-        self.area = area
+        self.salary = salary
+        self.experience = experience
         self.link = link
 
     def __str__(self):
         return self.name
 
     def __repr__(self):
-        return f"{self.__class__.__name__}({self.name}, {self.area}, {self.link})"
+        return f"{self.__class__.__name__}({self.name}, {self.link}, {self.salary}, {self.experience})"
 
 
 hh_api = HeadHunterAPI()
 vacancies = hh_api.get_vacancies("Python")
-# print(repr(vacancies[0]))
-# for vacancy in vacancies:
-#     print(vacancy.name, vacancy.area, vacancy.link, sep="\t")
+sj_api = SuperJobAPI()
+vacancies.extend(sj_api.get_vacancies("Python"))
+for vacancy in vacancies:
+    print(vacancy)
+print(len(vacancies))
